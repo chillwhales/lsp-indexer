05e025d14a792e77e25369a5f22f3eb9d71f1331
default <b00ste.lyx@gmail.com>
2026-02-20 14:08:42 +0000

    feat(09.2-03): wire NFT exports through react and next entry points
    - Add NFT hook exports to @lsp-indexer/react index
    - Add NFT server action and hook exports to @lsp-indexer/next index
    - All 4 packages (types, node, react, next) build successfully
    - NFT hooks available from both @lsp-indexer/react and @lsp-indexer/next

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,4 +1,6 @@
 export * from './actions/digital-assets';
+export * from './actions/nfts';
 export * from './actions/profiles';
 export * from './hooks/digital-assets';
+export * from './hooks/nfts';
 export * from './hooks/profiles';
