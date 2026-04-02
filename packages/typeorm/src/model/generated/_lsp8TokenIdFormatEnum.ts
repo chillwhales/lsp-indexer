bc2a2265ac9fd47251d7222d03ee0ee237ecde73
default <b00ste.lyx@gmail.com>
2026-04-02 10:15:10 +0000

    fix: remove accidentally tracked legacy packages/abi and packages/typeorm files

--- packages/typeorm/src/model/generated/_lsp8TokenIdFormatEnum.ts
+++
@@ -1,6 +0,0 @@
-export enum LSP8TokenIdFormatEnum {
-    NUMBER = "NUMBER",
-    STRING = "STRING",
-    ADDRESS = "ADDRESS",
-    BYTES32 = "BYTES32",
-}
--- packages/typeorm/src/model/generated/_operationType.ts
+++
@@ -1,7 +0,0 @@
-export enum OperationType {
-    CALL = "CALL",
-    CREATE = "CREATE",
-    CREATE2 = "CREATE2",
-    STATICCALL = "STATICCALL",
-    DELEGATECALL = "DELEGATECALL",
-}
